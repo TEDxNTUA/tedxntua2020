e90f3b0c00a1934d19b51e2f68a96bdbb287bf53
nick bellos <nikolas.bellos@gmail.com>
2020-02-22 23:58:00 +0200

    insignificant change in feed

--- project/feed/views.py
+++ project/feed/views.py
@@ -4,7 +4,7 @@ from django.views import View
 class FeedView(View):
     template = 'feed/index.html'
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         return render(request, self.template)
 
 # Create your views here.
