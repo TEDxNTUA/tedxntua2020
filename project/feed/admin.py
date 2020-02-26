db933cf83a7fe45b9f2371b96e993cba42f17c21
nick bellos <nikolas.bellos@gmail.com>
2020-02-27 00:20:27 +0200

    live feed template for small screens

--- project/feed/admin.py
+++ project/feed/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from project.utils.admin import PartiallyTranslatableAdmin, render_link_field
+from .models import Notification
+
+class NotificationAdmin(PartiallyTranslatableAdmin):
+    list_display = ('__str__', 'created_at', 'title')
+    # ordering = ['start']
+
+    def link_html(self, obj):
+        return render_link_field(obj, 'link', new_tab=True)
+    link_html.short_description = ('Link')
+
+admin.site.register(Notification, NotificationAdmin)
 # Register your models here.
--- project/feed/models.py
+++ project/feed/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+import datetime
+
+from parler.models import TranslatableModel, TranslatedFields, TranslationDoesNotExist
+from parler.managers import TranslatableQuerySet, TranslatableManager
+
+class ActivityTypeManager(TranslatableManager):
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            is_published=True,
+        )
+
+class Notification(TranslatableModel):
+
+    # GENERAL = 'G'
+    # TALK = 'T'
+    # PERFORMANCE = 'P'
+    # SIDE_EVENT = 'S'
+    # HOSTING = 'H'
+    # SPONSOR = 'SP'
+    # TYPE_CHOICES = (
+    #     (GENERAL, _('General')),
+    #     (TALK, _('Talk')),
+    #     (PERFORMANCE, _('Performance')),
+    #     (SIDE_EVENT, _('Side event')),
+    #     (HOSTING, _('Hosting')),
+    #     (SPONSOR, _('Sponsor')),
+    # )
+
+    # message_type = models.CharField(max_length=1, choices=TYPE_CHOICES,
+    #                                  verbose_name='Type')
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    translations = TranslatedFields(
+        title=models.CharField(max_length=255),
+        message=models.TextField(),
+    )
+
+    # is_published = models.BooleanField(_('Published'), default=True)
+
+    # user_id = models.ForeignKey(
+    #     'User',
+    #     null=True,
+    #     on_delete=models.SET_NULL,
+    # )
 
 # Create your models here.
