3739ebbaf17b0d8f5afae3e028a028426e3f00a6
Theodore Diamantidis <diamaltho@gmail.com>
2020-02-29 21:04:14 +0200

    Prelaunch fixes 01 (#41)
    * fix(home): Hide about header when footer is visible
    
    * improvement(home): Merge all venue info to a single 'tab' to fill space
    
    * feat: Fill URLs
    
    Fill nav and footer links with URLs
    Set tickets URL
    
    * feat(nav): Add speakers, performers icons
    
    * feat: Make language switches work in nav, footer
    
    * assets: Replace dualism logo with new with transparent background

--- project/templatetags/lang_extras.py
+++ project/templatetags/lang_extras.py
@@ -3,6 +3,7 @@ Relevant Django docs:
 https://docs.djangoproject.com/en/2.2/howto/custom-template-tags/
 '''
 from django import template
+from django.urls import translate_url
 
 
 register = template.Library()
@@ -14,3 +15,17 @@ def lang_short_name(code):
       'en': 'EN',
     }
     return short_names.get(code, '')
+
+@register.simple_tag(takes_context=True)
+def change_lang(context, lang=None, *args, **kwargs):
+    """
+    Returns the translated version of the current URL.
+
+    Example:
+        <a href="{% change_lang 'en' %}">English version</a>
+
+    Adapted from:
+    https://stackoverflow.com/a/57578343
+    """
+    path = context['request'].path
+    return translate_url(path, lang)
